da37e2ce43d73baea9059a1d79bf68dd3db0becf
Deepanshu7517 <deepudagar90@gmail.com>
2025-04-21 15:33:14 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import StopWatch from './StopWatch';
 export default function App() {
   return (
     <h1 className="w-screen h-screen">
-      {/* <PromodoroTimer /> */}
       <StopWatch />
     </h1>
   )
